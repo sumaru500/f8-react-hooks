6263419978a20b9214602b067e3e67f8a4e7f3cc
sumaru500 <phamthang37@yahoo.com>
2022-07-27 22:03:26 +0200

    Lack combine store = useContext + useReducer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import CounterUseState from './components/useState/CounterUseState';
 import GiftUseState from './components/useState/GiftUseState';
 import TwoWayBindingUseState from './components/useState/TwoWayBindingUseState';
 import TodoListUseState from './components/useState/TodoListUseState';
+import ContentUseEffect from './components/useEffect/ContentUseEffect';
+import AvatarCleanUpUseEffect from './components/useEffect/AvatarCleanUpUseEffect';
+import ChatCleanUpUseEffect from './components/useEffect/ChatCleanUpUseEffect';
+import CounterUseLayoutEffect from './components/useLayoutEffect/CounterUseLayoutEffect';
+import CountDownUseRef from './components/useRef/CountDownUseRef';
+import CounterMemo from './components/memo/CounterMemo';
+import CounterUseCallback from './components/useCallback/CounterUseCallback';
+import ProductUseMemo from './components/useMemo/ProductUseMemo';
+import CounterUseReducer from './components/useReducer/CounterUseReducer';
+import TodoUseReducer from './components/useReducer/TodoUseReducer';
+import PageUseContext from './components/useContext/PageUseContext';
 
 function App() {
   return (
@@ -15,6 +26,28 @@ function App() {
         <TwoWayBindingUseState></TwoWayBindingUseState>
 
         <TodoListUseState></TodoListUseState>
+
+        <ContentUseEffect></ContentUseEffect>
+
+        <AvatarCleanUpUseEffect></AvatarCleanUpUseEffect>
+
+        <ChatCleanUpUseEffect></ChatCleanUpUseEffect>
+
+        <CounterUseLayoutEffect></CounterUseLayoutEffect>
+
+        <CountDownUseRef></CountDownUseRef>
+
+        <CounterMemo></CounterMemo>
+
+        <CounterUseCallback></CounterUseCallback>
+
+        <ProductUseMemo></ProductUseMemo>
+
+        <CounterUseReducer></CounterUseReducer>
+
+        <TodoUseReducer></TodoUseReducer>
+
+        <PageUseContext></PageUseContext>
     </div>
   );
 }
